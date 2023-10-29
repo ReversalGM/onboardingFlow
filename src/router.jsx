742e376b39daca25e7c826f2bc07a898d450f881
ReversalGM <zhouhyan.steven@gmail.com>
2023-10-29 17:08:50 -0400

    Change route to root

--- src/router.jsx
+++ src/router.jsx
@@ -2,5 +2,5 @@ import { createBrowserRouter } from "react-router-dom"
 import { Onboarding } from "./Onboarding/Onboarding"
 
 export const router = createBrowserRouter([
-  { path: "/onboarding", element: <Onboarding /> },
+  { path: "/", element: <Onboarding /> },
 ])
