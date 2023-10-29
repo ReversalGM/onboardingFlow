69e69b832d71837cd668a4f61851cf76b9886467
ReversalGM <zhouhyan.steven@gmail.com>
2023-10-29 17:55:08 -0400

    Update basename in router

--- src/router.jsx
+++ src/router.jsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from "react-router-dom"
 import { Onboarding } from "./Onboarding/Onboarding"
 
-export const router = createBrowserRouter([
-  { path: "/onboardingFlow/", element: <Onboarding /> },
-])
+export const router = createBrowserRouter(
+  [{ path: "/", element: <Onboarding /> }],
+  { basename: "/onboardingFlow/" }
+)
